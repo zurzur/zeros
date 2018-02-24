6d1a5a15ac0990afcb3dc402731fd7af2ea73f4a
zurzur <@gmail.com>
2018-02-24 09:19:30 +0300

    zeros/feature: implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let counter = 0;
+  for (let i = number; i >= 5; i--) {
+  	let j = i;
+  	while (!(j%5)) {
+  		counter++;
+  		j = j/5;
+  	}
+  }
+  return counter;
 }
