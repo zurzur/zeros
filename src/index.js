08b82c060ad288ce005bf8a00f13737ac264c48e
zurzur <@gmail.com>
2018-02-24 09:57:21 +0300

    zeros/fix: faster

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 module.exports = function getZerosCount(number) {
   let counter = 0;
-  for (let i = number; i >= 5; i--) {
-  	let j = i;
+  let i;
+  let j;
+  for (i = number; i >= 5; i--) {
+  	j = i;
   	while (!(j%5)) {
-  		counter++;
+  		
   		j = j/5;
+  		counter++;
   	}
   }
   return counter;
